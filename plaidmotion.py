08afe37c496e6e6de2111891468fe7e9b75bcf5a
Beibaibaby <56213541+Beibaibaby@users.noreply.github.com>
2021-04-09 19:46:25 +0800

    Update plaidmotion.py

--- plaidmotion.py
+++ plaidmotion.py
@@ -1,96 +1,200 @@
+from __future__ import absolute_import, division
 
-from psychopy import core, visual, gui, data, event
-from psychopy.tools.filetools import fromFile, toFile
-import numpy as np
-import random
+from psychopy import locale_setup
+from psychopy import prefs
+from psychopy import sound, gui, visual, core, data, event, logging, clock, colors, parallel
+from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
+                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
 
-try:  # try to get a previous parameters file
-    expInfo = fromFile('lastParams.pickle')
-except:  # if not there then use a default set
-    expInfo = {'observer':'jwp', 'refOrientation':0}
-expInfo['dateStr'] = data.getDateStr()  # add the current time
-# present a dialogue to change params
-dlg = gui.DlgFromDict(expInfo, title='simple JND Exp', fixed=['dateStr'])
-if dlg.OK:
-    toFile('lastParams.pickle', expInfo)  # save params to file for next time
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+from numpy.random import random, randint, normal, shuffle, choice as randchoice
+import os  # handy system and path functions
+import sys  # to get file system encoding
+
+from psychopy.hardware import keyboard
+
+
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Store info about the experiment session
+psychopyVersion = '2021.1.2'
+expName = 'k2'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001'}
+dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath='/Users/dragon/Desktop/CN/PLAID/k2.py',
+    savePickle=True, saveWideText=True,
+    dataFileName=filename)
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.DEBUG)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+frameTolerance = 0.001  # how close to onset before 'same' frame
+
+# Start Code - component code to be run after the window creation
+
+# Setup the Window
+win = visual.Window(
+    size=[1536, 960], fullscr=True, screen=0, 
+    winType='pyglet', allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True, 
+    units='height')
+# store frame rate of monitor if we can measure it
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] != None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
 else:
-    core.quit()  # the user hit cancel so exit
+    frameDur = 1.0 / 60.0  # could not measure, so guess
 
-# make a text file to save data
-fileName = expInfo['observer'] + expInfo['dateStr']
-dataFile = open(fileName+'.csv', 'w')  # a simple text file with 'comma-separated-values'
-dataFile.write('targetSide,oriIncrement,correct\n')
+# create a default keyboard (e.g. to check for escape)
+defaultKeyboard = keyboard.Keyboard()
 
-# create the staircase handler
-staircase = data.StairHandler(startVal = 20.0,
-                          stepType = 'db', stepSizes=[8,4,4,2],
-                          nUp=1, nDown=3,  # will home in on the 80% threshold
-                          nTrials=1)
-
-
-# create window and stimuli
-myWin = visual.Window((600, 600), allowGUI=True,
-                      monitor='testMonitor', units='deg')
-
-grating1 = visual.GratingStim(myWin, mask="raisedCos", color=[1.0, 1.0, 1.0], opacity=1.0, size=(1.5, 1.5), sf=(7, 0),
-                              ori=45)
-
-grating2 = visual.GratingStim(myWin, mask="raisedCos", color=[1.0, 1.0, 1.0], opacity=0.5, size=(1.5, 1.5), sf=(7, 0),
-                              ori=135)
-
-# and some handy clocks to keep track of time
-globalClock = core.Clock()
+# Initialize components for Routine "trial"
 trialClock = core.Clock()
+grating = visual.GratingStim(
+    win=win, name='grating',
+    tex='sin', mask=None,
+    ori=0.0, pos=(0, 0), size=(0.5, 0.5), sf=None, phase=0.0,
+    color=[1,1,1], colorSpace='rgb',
+    opacity=None, contrast=1.0, blendmode='avg',
+    texRes=128.0, interpolate=True, depth=0.0)
+key_resp = keyboard.Keyboard()
 
-# display instructions and wait
-message1 = visual.TextStim(win, pos=[0,+3],text='Hit a key when ready.')
-message2 = visual.TextStim(win, pos=[0,-3],
-    text="Then press left or right to identify the %.1f deg probe." %expInfo['refOrientation'])
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
 
-
+# ------Prepare to start Routine "trial"-------
+continueRoutine = True
+# update component parameters for each repeat
+key_resp.keys = []
+key_resp.rt = []
+_key_resp_allKeys = []
+# keep track of which components have finished
+trialComponents = [grating, key_resp]
+for thisComponent in trialComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
 t = 0
-thisResp=None
-while t < 20:
-        t = trialClock.getTime()
-        grating1.setPhase(2 * t)
-        grating1.draw()  # redraw it
-        grating2.setPhase(2 * t)
-        grating2.draw()  # redraw it
-        myWin.flip()
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+trialClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
 
-        while thisResp==None:
-         allKeys=event.waitKeys()
-         for thisKey in allKeys:
-            if thisKey=='left':
-                thisResp = 1  # correct
-              # incorrect
-            elif thisKey=='right':
-                 thisResp = -1              # incorrect
-            elif thisKey in ['q', 'escape']:
-                core.quit()  # abort experiment
-        event.clearEvents()  # clear other (eg mouse) events - they clog the buffer
-        staircase.addData(thisResp)
-        dataFile.write('%i\n' %(thisResp))
-        core.wait(1)
+# -------Run Routine "trial"-------
+while continueRoutine:
+    # get current time
+    t = trialClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=trialClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *grating* updates
+    if grating.status == NOT_STARTED and tThisFlip >= 2.0-frameTolerance:
+        # keep track of start time/frame for later
+        grating.frameNStart = frameN  # exact frame index
+        grating.tStart = t  # local t and not account for scr refresh
+        grating.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(grating, 'tStartRefresh')  # time at next scr refresh
+        grating.setAutoDraw(True)
+    if grating.status == STARTED:
+        # is it time to stop? (based on global clock, using actual start)
+        if tThisFlipGlobal > grating.tStartRefresh + 3.0-frameTolerance:
+            # keep track of stop time/frame for later
+            grating.tStop = t  # not accounting for scr refresh
+            grating.frameNStop = frameN  # exact frame index
+            win.timeOnFlip(grating, 'tStopRefresh')  # time at next scr refresh
+            grating.setAutoDraw(False)
+    
+    # *key_resp* updates
+    waitOnFlip = False
+    if key_resp.status == NOT_STARTED and tThisFlip >= 4.0-frameTolerance:
+        # keep track of start time/frame for later
+        key_resp.frameNStart = frameN  # exact frame index
+        key_resp.tStart = t  # local t and not account for scr refresh
+        key_resp.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(key_resp, 'tStartRefresh')  # time at next scr refresh
+        key_resp.status = STARTED
+        # keyboard checking is just starting
+        waitOnFlip = True
+        win.callOnFlip(key_resp.clock.reset)  # t=0 on next screen flip
+        win.callOnFlip(key_resp.clearEvents, eventType='keyboard')  # clear events on next screen flip
+    if key_resp.status == STARTED and not waitOnFlip:
+        theseKeys = key_resp.getKeys(keyList=['y', 'n', 'left', 'right', 'space'], waitRelease=False)
+        _key_resp_allKeys.extend(theseKeys)
+        if len(_key_resp_allKeys):
+            key_resp.keys = _key_resp_allKeys[-1].name  # just the last key pressed
+            key_resp.rt = _key_resp_allKeys[-1].rt
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
 
-# staircase has ended
-dataFile.close()
-staircase.saveAsPickle(fileName)  # special python binary file to save all the info
+# -------Ending Routine "trial"-------
+for thisComponent in trialComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('grating.started', grating.tStartRefresh)
+thisExp.addData('grating.stopped', grating.tStopRefresh)
+# check responses
+if key_resp.keys in ['', [], None]:  # No response was made
+    key_resp.keys = None
+thisExp.addData('key_resp.keys',key_resp.keys)
+if key_resp.keys != None:  # we had a response
+    thisExp.addData('key_resp.rt', key_resp.rt)
+thisExp.addData('key_resp.started', key_resp.tStartRefresh)
+thisExp.addData('key_resp.stopped', key_resp.tStopRefresh)
+thisExp.nextEntry()
+# the Routine "trial" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
 
-# give some output to user in the command line in the output window
-print('reversals:')
-print(staircase.reversalIntensities)
-approxThreshold = np.average(staircase.reversalIntensities[-6:])
-print('mean of final 6 reversals = %.3f' % (approxThreshold))
+# Flip one final time so any remaining win.callOnFlip() 
+# and win.timeOnFlip() tasks get executed before quitting
+win.flip()
 
-# give some on-screen feedback
-feedback1 = visual.TextStim(
-        win, pos=[0,+3],
-        text='mean of final 6 reversals = %.3f' % (approxThreshold))
-
-feedback1.draw()
-myWin.flip()
-event.waitKeys()  # wait for participant to respond
-
-myWin.close()
+# these shouldn't be strictly necessary (should auto-save)
+thisExp.saveAsWideText(filename+'.csv', delim='auto')
+thisExp.saveAsPickle(filename)
+logging.flush()
+# make sure everything is closed down
+thisExp.abort()  # or data files will save again on exit
+win.close()
 core.quit()
